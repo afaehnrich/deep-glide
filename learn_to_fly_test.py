36fb6a91c244db7e39bc9a758ab6096d7a62c7a7
afaehnrich <55191281+afaehnrich@users.noreply.github.com>
2020-12-17 20:49:05 +0100

    TOML-Konfiguration erweitert

--- learn_to_fly_test.py
+++ learn_to_fly_test.py
@@ -14,11 +14,12 @@ import random
 
 np.set_printoptions(precision=2, suppress=True)
 enable_fgfs = False
+max__travel_dist = 0.15
 
-cfg = toml.load('gym-jsbsim-cfg.toml')
+cfg = toml.load('heading-control.toml')
+#cfg = toml.load('fly-along-line.toml')
 
-env = NormalizedEnv(jsbgym_flex.environment.JsbSimEnv(cfg = cfg, 
-        task_type = AFHeadingControlTask, shaping = Shaping.STANDARD))
+env = NormalizedEnv(jsbgym_flex.environment.JsbSimEnv(cfg = cfg, shaping = Shaping.STANDARD))
 #env = NormalizedEnv(jsbgym_flex.environment.JsbSimEnv(cfg = cfg, 
 #        task_type = FlyAlongLineTask, shaping = Shaping.STANDARD))
 if torch.cuda.is_available():
@@ -26,17 +27,24 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 print('Torch Device: {}'.format(device))
-agent = DDPGagent(env, device)
+actor = cfg.get('environment').get('actor')
+critic = cfg.get('environment').get('critic')
+agent = DDPGagent(env, device, actor, critic)
 noise = OUNoise(env.action_space)
 batch_size = 128
 rewards = []
 avg_rewards = []
-p1 = plt.subplot(2,1,1)
-p2 = plt.subplot(2,1,2)
-p1.plot(0,0,'.')
+plt.figure()
+#p1 = plt.subplot(2,1,1)
+#p2 = plt.subplot(2,1,2)
+latstart = cfg.get('environment').get('initial_state').get('initial_latitude_geod_deg')
+lonstart = cfg.get('environment').get('initial_state').get('initial_longitude_geoc_deg')
+plt.plot(0,0,'.')
+plt.figure()
 plt.ion()
 plt.show()   
 plt.pause(0.001)        
+t_head=0
 for episode in range(0,150,1):
     state = env.reset()
     noise.reset()
@@ -44,6 +52,7 @@ for episode in range(0,150,1):
     env.set_property('heading_deg', random.randrange(0,360,1))
     routex=[]
     routey=[]
+    #env.set_property('target_heading', t_head)
     for step in range(500):
         if step%10 == 0 or episode > 50:
             # Am Anfang actions wiederholen, um 
@@ -67,16 +76,22 @@ for episode in range(0,150,1):
             sys.stdout.write("episode: {}, reward: {}, average _reward: {:.2f} \n".
                     format(episode, np.round(episode_reward, decimals=2), 
                     np.mean(rewards[-10:])))
-            p1.clear()
-            p1.plot(routex,routey,'.')
+            plt.figure(1)
+            plt.clf()
+            plt.xlim(latstart-max__travel_dist, latstart+max__travel_dist)
+            plt.ylim(lonstart-max__travel_dist, lonstart+max__travel_dist)
+            plt.plot(routex,routey,'.')
             plt.show()
             plt.pause(0.001)        
+          #  print ('target_head = {}'.format(t_head))
+           # t_head += 0.1
             break
     rewards.append(episode_reward)
     avg_rewards.append(np.mean(rewards[-10:]))
-    p2.clear()
-    p2.plot(rewards)
-    p2.plot(avg_rewards)
+    plt.figure(2)
+    plt.clf()
+    plt.plot(rewards)
+    plt.plot(avg_rewards)
 
 plt.ioff()
 plt.show()
