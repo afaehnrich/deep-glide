d05bf78321a263ed02bb7151c64f5c3c4f916f9d
afaehnrich <a.faehnrich.acc@gmail.com>
2021-08-25 11:25:48 +0200

    docker scripts

---
+++ Docker/train/docker_custom_zoo_train_v6_sde_8.bat
@@ -0,0 +1,6 @@
+docker run --mount src=%~dp0/../../../rl-baselines3-zoo/logs,target=/root/code/rl-baselines3-zoo/logs,type=bind ^
+			--mount src=%~dp0/../../../rl-baselines3-zoo/rl-trained-agents,target=/root/code/rl-baselines3-zoo/rl-trained-agents,type=bind ^
+			--mount src=%~dp0/../../../rl-baselines3-zoo/logs_enjoy,target=/root/code/rl-baselines3-zoo/logs_enjoy,type=bind ^
+       afaehnrich/deep-glide-custom-zoo:latest python3 train.py --algo sac --env JSBSim-v6 -n 1000000 --eval-freq 10000 --save-freq 50000 --tensorboard-log logs ^
+			--hyperparams sde_sample_freq:8 use_sde_at_warmup:True
+			
